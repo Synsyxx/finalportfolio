249de608537397b51171cf0607cdfd80290a3871
Travis Simpson <travis.simpson92@hotmail.com>
2019-04-02 12:49:52 -0400

    experimenting with different animations

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,15 +7,22 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hello</h1>
-    <p>My name is Travis Simpson.</p>
-    <p>I'm a Front End Web Developer.</p>
-    <p>I love to travel.</p>
-    <p>And ride motorcycles.</p>
-    <p>I'm also an avid Coffee Addict.</p>
-    {/*<div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-</div> */}
+      <div class="fade"></div>
+      <section class="star-wars">
+        <div class="crawl">
+          <div class="title">
+            <h1>Hello</h1>
+            <p>My name is Travis Simpson.</p>
+          </div>
+          <p>I'm a Front End Web Developer.</p>
+          <p>I love to travel.</p>
+          <p>And ride motorcycles.</p>
+          <p>I'm also an avid Coffee Addict.</p>
+        </div>
+      {/*<div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Image />
+  </div> */}
+      </section>
   </Layout>
 )
 
