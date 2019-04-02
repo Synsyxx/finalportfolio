70c36d8060743a378afae56b74d7b13f10279f54
Travis Simpson <travis.simpson92@hotmail.com>
2019-04-02 12:59:54 -0400

    more css animation changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,22 +7,15 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-      <div class="fade"></div>
-      <section class="star-wars">
-        <div class="crawl">
-          <div class="title">
-            <h1>Hello</h1>
-            <p>My name is Travis Simpson.</p>
-          </div>
-          <p>I'm a Front End Web Developer.</p>
-          <p>I love to travel.</p>
-          <p>And ride motorcycles.</p>
-          <p>I'm also an avid Coffee Addict.</p>
-        </div>
+          <h1 class="grow">Hello</h1>
+          <p class="grow">My name is Travis Simpson.</p>
+          <p class="grow">I'm a Front End Web Developer.</p>
+          <p class="grow">I love to travel.</p>
+          <p class="grow">And ride motorcycles.</p>
+          <p class="grow">I'm also an avid Coffee Addict.</p>
       {/*<div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
         <Image />
   </div> */}
-      </section>
   </Layout>
 )
 
